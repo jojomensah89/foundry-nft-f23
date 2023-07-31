5ffec9a54993ca2e92e6ee7ca717729b8a45babf
jojomensah89 <jojomensah89@gmail.com>
2023-07-31 10:25:24 +0000

    wrote tests for MoodNft

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -3,15 +3,18 @@
 pragma solidity ^0.8.18;
 
 import {MoodNft} from "../src/MoodNft.sol";
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script {
     MoodNft moodNft;
 
     function run() external returns (MoodNft) {
+        string memory sadSvg = vm.readFile("./images/sad.svg");
+        string memory happySvg = vm.readFile("./images/happy.svg");
+        // console.log(sadSvg,happySvg);
         vm.startBroadcast();
-        moodNft = new MoodNft();
+        moodNft = new MoodNft(svgToImageURI(sadSvg),svgToImageURI(happySvg));
         vm.stopBroadcast();
 
         return (moodNft);
@@ -20,7 +23,7 @@ contract DeployMoodNft is Script {
     function svgToImageURI(string memory svg) public pure returns (string memory) {
         string memory baseURL = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
-
+        // concat baseURL and base64 encoded svg
         return string(abi.encodePacked(baseURL, svgBase64Encoded));
     }
 }
--- test/DeployMoodNftTest.t.sol
+++ test/DeployMoodNftTest.t.sol
@@ -15,4 +15,24 @@ contract DeployMoodNftTest is Test {
         DeployMoodNft deployer = new DeployMoodNft();
         moodNft = deployer.run();
     }
+
+    function testConvertSvgToUri() public {
+        //         string memory expectedUri = 'data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjAwIDIwMCIgd2lkdGg9IjQwMCIgIGhlaWdodD0iNDAwIiB4bWxu
+        // cz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgogIDxjaXJjbGUgY3g9IjEwMCIgY3k9IjEw
+        // MCIgZmlsbD0ieWVsbG93IiByPSI3OCIgc3Ryb2tlPSJibGFjayIgc3Ryb2tlLXdpZHRoPSIzIi8+
+        // CiAgPGcgY2xhc3M9ImV5ZXMiPgogICAgPGNpcmNsZSBjeD0iNjEiIGN5PSI4MiIgcj0iMTIiLz4K
+        // ICAgIDxjaXJjbGUgY3g9IjEyNyIgY3k9IjgyIiByPSIxMiIvPgogIDwvZz4KICA8cGF0aCBkPSJt
+        // MTM2LjgxIDExNi41M2MuNjkgMjYuMTctNjQuMTEgNDItODEuNTItLjczIiBzdHlsZT0iZmlsbDpu
+        // b25lOyBzdHJva2U6IGJsYWNrOyBzdHJva2Utd2lkdGg6IDM7Ii8+Cjwvc3ZnPg==';
+        // string memory svg = '<svg viewBox="0 0 200 200" width="400"  height="400" xmlns="http://www.w3.org/2000/svg">
+        //   <circle cx="100" cy="100" fill="yellow" r="78" stroke="black" stroke-width="3"/>
+        //   <g class="eyes">
+        //     <circle cx="61" cy="82" r="12"/>
+        //     <circle cx="127" cy="82" r="12"/>
+        //   </g>
+        //   <path d="m136.81 116.53c.69 26.17-64.11 42-81.52-.73" style="fill:none; stroke: black; stroke-width: 3;"/>
+        // </svg>';
+        // string memory actualUri =deployer.svgToImageURI(svg);
+        // assert(keccak256(abi.encodePacked(actualUri)) === keccak256(abi.encodePacked(expectedUri)) );
+    }
 }
